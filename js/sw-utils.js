02a765cbe67ecec59db382dbeb015f875eb7351e
root <root@F-01-TEC-LAP-02.fondesaong.local>
2022-03-23 13:29:12 -0400

    Primer Commit

---
+++ js/sw-utils.js
@@ -0,0 +1,12 @@
+
+
+function actualizaCacheDinamica(dynamicCache, req, res) {
+    if (res.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        })
+    } else {
+        return res;
+    }
+}
