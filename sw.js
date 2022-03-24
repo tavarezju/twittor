dba92d03bbf45dc664a90f4b53bf1024fa78b614
Juan Tavarez <tavarezju@fondesa.org>
2022-03-24 11:17:52 -0400

    SW twittor hoy

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 
-importScripts('js/sw-utils.js')
+importScripts('/js/sw-utils.js')
 
 const STATIC_CACHE = 'static-v1'
 const DYNAMIC_CACHE = 'dynamic-v1'
 const INMUTABLE_CACHE = 'inmutable-v1'
 
 const APP_SHELL = [
-    '/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
@@ -21,7 +21,7 @@ const APP_SHELL = [
 const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
-    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'https://kit.fontawesome.com/6a7ae3e42f.js',
     'css/animate.css',
     'js/libs/jquery.js',
     'js/sw-utils.js'
